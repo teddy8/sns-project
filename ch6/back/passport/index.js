f27fa45463e319c24252ff59b40cb8269c75af09
ohhanmin <ohanmin9@gmail.com>
2020-08-15 17:50:21 +0900

    commit test 20200816

--- ch6/back/passport/index.js
+++ ch6/back/passport/index.js
@@ -3,23 +3,42 @@ const db = require('../models');
 const local = require('./local')
 
 module.exports = () => {
+    console.log('[]A'); // 로그인 전 (1/4)
     
     passport.serializeUser((user, done) => {
+        console.log('[]B'); // 로그인 후 (2/2)
         return done(null, user.id);
     })
 
     passport.deserializeUser(async (id, done) => {
+        console.log('[]C');
         try {
             const user = await db.User.findOne({
                 where: { id },
+                include: [{
+                    model: db.Post,
+                    as: 'Posts',
+                    attributes: ['id'],
+                }, {
+                    model: db.User,
+                    as: 'Followings',
+                    attributes: ['id'],
+                }, {
+                    model: db.User,
+                    as: 'Followers',
+                    attributes: ['id'],
+                },]
             });
-
+            console.log('[]D');
             return done(null, user); // req.user에 저장
         } catch (e) {
+            console.log('[]E');
             console.error(e);
             return done(e);
         }
     })
 
-    local();
+    console.log('[]F'); // 로그인 전 (2/4)
+    local(); // H 로그인 전 (3/4)
+    console.log('[]G'); // // 로그인 전 (4/4)
 };
--- ch6/back/passport/local.js
+++ ch6/back/passport/local.js
@@ -4,23 +4,26 @@ const bcrypt = require('bcrypt');
 const db = require('../models');
 
 module.exports = () => {
+    console.log('[]H');
     passport.use(new LocalStrategy({
         usernameField: 'userId',
         passwordField: 'password',
     }, async (userId, password, done) => {
+        console.log('[]I'); // 로그인 후  (1/2)
         try {
             const user = await db.User.findOne({ where: { userId }});
             if (!user) {
                 return done(null, false, { reason: '존재하지 않는 사용자입니다.'});
             }
+
             const result = await bcrypt.compare(password, user.password); // 비밀번호 일치여부
-            if (result) {
-                // console.log('user1 = ' + JSON.stringify(user));
-                
-                return done(null, user);
+            if (!result) {
+                return done(null, false, {reason: '비밀번호가 틀립니다.'})
             }
-            return done(null, false, {reason: '비밀번호가 틀립니다.'})
+            
+            return done(null, user);
         } catch (e) {
+            console.log('[]K');
             console.error(e);
             return done(e);
         }
