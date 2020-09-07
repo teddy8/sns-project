0895e4a151775564572df1865bc108c6f26f6685
ohhanmin <ohanmin9@gmail.com>
2020-09-07 12:37:12 +0900

    [Modified] next.config.js AND package.json

--- ch8/front/next.config.js
+++ ch8/front/next.config.js
@@ -1,37 +1,19 @@
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 });
-const CompressionPlugin = require('compression-webpack-plugin');
 
 module.exports = withBundleAnalyzer({
   compress: true,
-  distDir: '.next',
-  analyzeServer: ['server', 'both'].includes(process.env.BUNDLE_ANALYZE),
-  analyzeBrowser: ['browser', 'both'].includes(process.env.BUNDLE_ANALYZE),
-  bundleAnalyzerConfig: {
-    server: {
-      analyzerMode: 'static',
-      reportFilename: '../bundles/server.html',
-    },
-    browser: {
-      analyzerMode: 'static',
-      reportFilename: '../bundles/client.html',
-    },
-  },
   webpack(config, { webpack }) {
     const prod = process.env.NODE_ENV === 'production';
-    const plugins = [
-      ...config.plugins,
-      new webpack.ContextReplacementPlugin(/moment[/\\]locale$/, /^\.\/ko$/),
-    ];
-    if (prod) {
-      plugins.push(new CompressionPlugin());
-    }
     return {
       ...config,
       mode: prod ? 'production' : 'development',
       devtool: prod ? 'hidden-source-map' : 'eval',
-      plugins,
+      plugins: [
+        ...config.plugins,
+        new webpack.ContextReplacementPlugin(/moment[/\\]locale$/, /^\.\/ko$/),
+      ],
     };
   },
 });
