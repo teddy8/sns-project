2ff3da4b8bb0cb19c406cb1c9690604837d81320
ohhanmin <ohanmin9@gmail.com>
2020-09-09 22:27:33 +0900

    [Modified] baseURL

--- ch8/front/sagas/index.ts
+++ ch8/front/sagas/index.ts
@@ -3,7 +3,8 @@ import axios from 'axios';
 import user from './user';
 import post from './post';
 
-axios.defaults.baseURL = 'http://localhost:3065/api';
+// axios.defaults.baseURL = 'http://localhost:3065/api';
+axios.defaults.baseURL = 'http://api.jinukcloud.pe.kr/api';
 
 export default function* rootSaga() {
   yield all([
